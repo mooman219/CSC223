1947c7c17db0a108917315b82c4aeec91a7e0040
LukkasFowl <Bstaffan@yahoo.com>
2013-10-08 16:00:26 -0700

    Committing to lab 4 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Lab3/ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -0,0 +1,39 @@
+/*
+Ben Staffan & Joe Cumbo
+Due: 9-26-2013
+Program Description:
+
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MONTHS_IN_YEAR = 12;
+
+void writeMonth(int a[], int monthNumber);
+
+void getInput(int a[], int rainAmount);
+
+void monthlyTotal(int& sum);
+
+void drawbar(const int asterisks[], int monthNumber);
+
+void drawScaleLine(int n);
+
+void statistics(int a[], int stats);
+
+int main();
+{
+    
+
+
+
+
+
+}
+
+
+
+
