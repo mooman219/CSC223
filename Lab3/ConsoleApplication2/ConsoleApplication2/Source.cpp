5a6713d4e775e4c02383564e80e952c26f8edcb2
Joe <mooman219@gmail.com>
2013-10-08 16:25:25 -0400

    Read input

--- Lab3/ConsoleApplication2/ConsoleApplication2/Source.cpp
+++ Lab3/ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -1,11 +1,12 @@
 /*
-Ben Staffan & Joe Cumbo
-Due: 9-26-2013
-Program Description:
-
-*/
+ Ben Staffan & Joe Cumbo
+ Due: 9-26-2013
+ Program Description:
+ */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <cmath>
 
 using namespace std;
@@ -25,14 +26,13 @@ void drawScaleLine(int n);
 void statistics(int a[], int stats);
 
 int main() {
-    
-
-
-
-
-
+    string line;
+    ifstream inputFile("rainInput.txt");
+    if (inputFile.is_open()) {
+        while (getline(inputFile, line)) {
+            cout << line << endl;
+        }
+        inputFile.close();
+    }
 }
-
-
-
 
