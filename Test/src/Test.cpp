8a5b4e5f15b336f23629f5649ebda26d11cb4fd0
Joe <mooman219@gmail.com>
2013-10-31 16:33:49 -0400

    testing

--- Test/src/Test.cpp
+++ Test/src/Test.cpp
@@ -1,15 +1,35 @@
-//============================================================================
-// Name        : Test.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+#include <istream>
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+
 using namespace std;
 
+enum InputType{INTEGER, DOUBLE, ADD, SUB, MULT, DIV, MOD, POW, TAN, SIN, COS, FIXED, SCI};
+
+void modeToPrint(char mode) {
+	switch(mode) {
+	case 'f':
+		break;
+	case 'e':
+		break;
+	default:
+		break;
+	}
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	char currentLine[50];
+    ifstream inputFile;
+    ofstream outputFile;
+    inputFile.open("PFN.txt");
+    outputFile.open("Output.txt");
+    while(inputFile.getline(currentLine, 50)) {
+    	for(int index = 0; currentLine[index] != '\0'; index++){
+    		cout << currentLine[index];
+    	}
+    	cout << endl;
+    }
 	return 0;
 }
